f8fed58a71a53a07d88c2a76407faf122ef541a6
cdcl10 <62439462+cdcl10@users.noreply.github.com>
2021-06-17 16:32:22 -0500

    Documentaion

--- src/main/java/edu/unbosque/ProyectoFinal_backend/jpa/Repositories/UserAppRepositoryImpl.java
+++ src/main/java/edu/unbosque/ProyectoFinal_backend/jpa/Repositories/UserAppRepositoryImpl.java
@@ -7,20 +7,32 @@ import javax.persistence.EntityManager;
 import java.util.Optional;
 
 public class UserAppRepositoryImpl implements UserAppRepository {
-
+/*/
+    La entity manager es capaz de trabajar con entidades gestionadas (Managed).
+ */
     private EntityManager entityManager;
 
     public UserAppRepositoryImpl(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
+/*/
+    El Override que se utuliza para sobre escribir metodos virtuales o implementar metodos ya sean abatractos
 
+ */
     @Override
     public Optional<UserApp> findByUsername(String username) {
         UserApp user = entityManager.find(UserApp.class, username);
         return user != null ? Optional.of(user) : Optional.empty();
     }
 
+    /*/
+    El save lo que hace es guardar de manera alterar la base de datos que se esta manejando
+    Transaction es el API mediante el cual es posible controlar las transacciones de una aplicación
+    Hace persistente una instancia transitoria. Sin embargo
+     */
+
     @Override
+
     public Optional<UserApp> save(UserApp user) {
         try {
             entityManager.getTransaction().begin();
